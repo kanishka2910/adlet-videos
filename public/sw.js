a617c726c06cb86c17e214dcf96fe4e59f5024a3
kanishka2910 <56442094+kanishka2910@users.noreply.github.com>
2020-05-25 20:16:34 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,99 @@
+importScripts('/js/idb.js');
+importScripts('/js/utility.js');
+
+var CACHE_STATIC_NAME = 'static-v5';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/utility.js',
+  '/js/idb.js',
+  '/js/promise.js',
+  '/js/fetch.js',
+  ];
+
+  self.addEventListener('install', function (event) {
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+      caches.open(CACHE_STATIC_NAME)
+        .then(function (cache) {
+          console.log('[Service Worker] Precaching App Shell');
+          cache.addAll(STATIC_FILES);
+        })
+    )
+  });
+  
+  self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating Service Worker ....', event);
+    event.waitUntil(
+      caches.keys()
+        .then(function(keyList) {
+          return Promise.all(keyList.map(function(key) {
+            if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+              console.log('[Service Worker] Removing old cache.', key);
+              return caches.delete(key);
+            }
+          }));
+        })
+    );
+    return self.clients.claim();
+  });
+
+  function isInArray(string, array) {
+    var cachePath;
+    if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
+      console.log('matched ', string);
+      cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+    } else {
+      cachePath = string; // store the full request (for CDNs)
+    }
+    return array.indexOf(cachePath) > -1;
+  }
+    
+  self.addEventListener('fetch', function (event) {
+
+    var url = 'http://127.0.0.1:8000/api/v1/kiosks/5e3d0a07fb38ed1578b24f6b/ads';
+    if (event.request.url.indexOf(url) > -1) {
+      event.respondWith(fetch(event.request)
+        .then(function (res) {
+          var clonedRes = res.clone();
+          clearAllData('videos')
+            .then(function () {
+              return clonedRes.json();
+            })
+            .then(function (datas) {
+              for(var key in datas.data.data){
+                writeData('videos', datas.data.data[key])
+              }
+                
+            });
+          return res;
+        })
+      );
+    } else if (isInArray(event.request.url, STATIC_FILES)) {
+      event.respondWith(
+        caches.match(event.request)
+      );
+    } else {
+      event.respondWith(
+        caches.match(event.request)
+          .then(function (response) {
+            if (response) {
+              return response;
+            } else {
+              return fetch(event.request)
+                .then(function (res) {
+                  return caches.open(CACHE_DYNAMIC_NAME)
+                    .then(function (cache) {
+                      // trimCache(CACHE_DYNAMIC_NAME, 3);
+                      cache.put(event.request.url, res.clone());
+                      return res;
+                    })
+                })
+            }
+          })
+      );
+    }
+  });
+  
